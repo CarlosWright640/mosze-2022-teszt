594687fb17ee78e8b2fdedfbe459f916c28d1a6f
Carlos Wright <r_dorian@freemail.hu>
2022-09-09 15:24:39 +0200

    Hibák kommentelése
    A main.cpp-n belül kommentelve lettek a hibák.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
-
+//kicsit konnyebb atlatni a kodot, ha alkalmazom a using namesapce std; -t
 constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS es kerek zarojel szogletes helyett.
+    std::cout << '1-100 ertekek duplazasa' //helyes verzio: "1-100 ertekek duplazasa"; 
+    for (int i = 0;) // a pontosvesszo utan: i<N_ELEMENTS+1; i++)
     {
-        b[i] = i * 2;
+        b[i] = i * 2; //ide beirhato az atlag += b[i];
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // ez a for ciklus jelenleg nem csinal semmit.
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hianyzik a pontos vesszo. ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //felesleges 3x ugyan azt a ciklus lefuttatni ugyan azokkal a parameterekkel.
     {
-        atlag += b[i]
+        atlag += b[i] 
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
