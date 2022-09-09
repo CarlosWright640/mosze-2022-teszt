c97b5025e344cb3d88fc505da3dfa75c29cf77ad
Carlos Wright <r_dorian@freemail.hu>
2022-09-09 15:40:39 +0200

    Javított kód
    A kód sokkal rövidebb lett, és most már működik is. Az átlag 101 lesz.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
-//kicsit konnyebb atlatni a kodot, ha alkalmazom a using namesapce std; -t
-constexpr int N_ELEMENTS = 100;
+using namespace std;
+const int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //NELEMENTS helyett N_ELEMENTS es kerek zarojel szogletes helyett.
-    std::cout << '1-100 ertekek duplazasa' //helyes verzio: "1-100 ertekek duplazasa"; 
-    for (int i = 0;) // a pontosvesszo utan: i<N_ELEMENTS+1; i++)
+    int *b = new int(N_ELEMENTS);
+    cout << "1-100 ertekek duplazasa."<<endl;
+    cout << "Ertek:"<<"\n";
+     float atlag=0.0;
+    for (int i = 0;i<N_ELEMENTS+1; i++)
     {
-        b[i] = i * 2; //ide beirhato az atlag += b[i];
+        b[i] = i * 2;
+        atlag += b[i];
+        cout<<i<<". szam: "<<b[i]<<"\n";
     }
-    for (int i = 0; i; i++) // ez a for ciklus jelenleg nem csinal semmit.
-    {
-        std::cout << "Ertek:" // hianyzik a pontos vesszo. ;
-    }    
-    std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) //felesleges 3x ugyan azt a ciklus lefuttatni ugyan azokkal a parameterekkel.
-    {
-        atlag += b[i] 
-    }
+    cout << "Atlag szamitasa: " << endl;
     atlag /= N_ELEMENTS;
-    std::cout << "Atlag: " << atlag << std::endl;
-    return 0;
+    cout << "Atlag: " << atlag << endl;
+  return 0;
 }
